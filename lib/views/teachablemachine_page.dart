23defae4b0da4b693bf38547f649ce0dbe1c5aea
ITSTIME <itstime0809@gmail.com>
2021-12-29 23:05:31 +0900

    (Update) Bloc Pattern 주석 수정, 추가 :)

--- lib/views/teachablemachine_page.dart
+++ lib/views/teachablemachine_page.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
+import 'package:fresh_app_teamproj/bloc/authentication_bloc.dart';
+import 'package:fresh_app_teamproj/bloc/bloc/login_bloc.dart';
 import 'package:fresh_app_teamproj/bloc/bloc/login_page.dart';
 
+//** TeachableMachine 위젯은 메인기능 페이지입니다.
+// 메인 페이지 기능은 아직 구현이 되지 않았습니다.
+// 구현이 되면 이 페이지에 추가될 예정입니다.
 class TeachableMachine extends StatefulWidget {
   @override
   _TeachableMachineState createState() => _TeachableMachineState();
