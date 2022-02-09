ddc2d4d7f752ec6d84b6b92cf6a9871362357a24
ITSTIME <itstime0809@gmail.com>
2022-02-10 02:04:04 +0900

    Update: Flutter version 2.2.3 & sdk version to change 30 & each other plugin version change :)

--- lib/data/model/loadTflite.dart
+++ lib/data/model/loadTflite.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:tflite/tflite.dart';
 
 class LoadModel {
   Future<void> loadTfliteModel() async {
+    // ignore: unused_local_variable
     final String? resource;
     try {
       resource = await Tflite.loadModel(
@@ -12,9 +14,7 @@ class LoadModel {
         numThreads: 5,
       );
     } catch (e) {
-      if (kDebugMode) {
-        print(e);
-      }
+      CircularProgressIndicator();
     }
   }
 }
